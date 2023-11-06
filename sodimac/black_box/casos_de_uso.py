bb56826f05f0dee8844034789b94f1884655f0a1
wladi <wladimir2000@protonmail.com>
2023-11-06 16:07:47 -0300

    Test unitarios según escenarios de calidad.

--- sodimac/black_box/casos_de_uso.py
+++ sodimac/black_box/casos_de_uso.py
@@ -3,3 +3,171 @@ def iniciar_sesion():
 
 def cerrar_sesion():
 	return True
+
+# Caso de prueba
+
+# Caso de prueba 1: Agregar Producto al Inventario
+def agregar_producto_al_inventario(nuevo_producto):
+    """
+    Simula la adición de un nuevo producto al inventario con valores válidos.
+    Verifica que el producto se haya agregado correctamente al inventario.
+    """
+    # Implementación simulada para caja negra
+    producto_agregado = True  # Simulamos que el producto se agrega con éxito.
+    return producto_agregado
+
+# Caso de prueba 2: Crear Pedido de Producto
+def crear_pedido_producto(datos_validos):
+    """
+    Simula la creación de un pedido de productos con datos válidos.
+    Verifica que el pedido se haya creado correctamente y afecte el inventario.
+    """
+    # Implementación simulada para caja negra
+    pedido_creado = True
+    inventario_afectado = True
+    return pedido_creado, inventario_afectado
+
+# Caso de prueba 3: Notificar Niveles Mínimos de Inventario
+def notificar_niveles_minimos_inventario(inventario_alcanza_minimos):
+    """
+    Simula la notificación a los administradores cuando el inventario alcanza niveles mínimos.
+    Verifica que se envíe una notificación a los administradores.
+    """
+    # Implementación simulada para caja negra
+    notificacion_enviada = True
+    return notificacion_enviada
+
+# Caso de prueba 4: Ejecutar Algoritmo de Reabastecimiento
+def ejecutar_algoritmo_reabastecimiento():
+    """
+    Simula la ejecución del algoritmo de reabastecimiento manualmente.
+    Verifica que el algoritmo determine de manera adecuada los productos que necesitan ser reabastecidos.
+    """
+    # Implementación simulada para caja negra
+    productos_reabastecidos = True
+    return productos_reabastecidos
+
+# Caso de prueba 5: Validar Cantidad de Producto en Orden de Compra
+def validar_cantidad_producto_orden_compra(cantidad):
+    """
+    Simula la validación de la cantidad de productos en una orden de compra.
+    Verifica que el sistema muestre un mensaje de error cuando se ingresa una cantidad no válida.
+    """
+    # Implementación simulada para caja negra
+    cantidad_valida = cantidad > 0
+    mensaje_error = "Cantidad no válida" if not cantidad_valida else ""
+    return cantidad_valida, mensaje_error
+
+# Caso de prueba 6: Calcular Disponibilidad de Productos
+def calcular_disponibilidad_productos(stock_disponible, pedidos_pendientes):
+    """
+    Simula el cálculo de la disponibilidad de productos en función del stock y los pedidos pendientes.
+    Verifica que la disponibilidad se calcule correctamente.
+    """
+    # Implementación simulada para caja negra
+    disponibilidad = stock_disponible - pedidos_pendientes
+    return disponibilidad
+
+# Caso de prueba 7: Aplicar Descuento por Nivel de Stock
+def aplicar_descuento_por_nivel_stock(nivel_stock):
+    """
+    Simula la aplicación de descuentos a productos en función de su nivel de stock.
+    Verifica que el descuento se aplique correctamente al precio del producto.
+    """
+    # Implementación simulada para caja negra
+    descuento_aplicado = nivel_stock >= 10
+    return descuento_aplicado
+
+# Caso de prueba 8: Sincronización con Proveedor
+def sincronizar_con_proveedor():
+    """
+    Simula la sincronización con el proveedor para mantener actualizado el inventario.
+    Verifica que la sincronización se complete con éxito y que los datos del inventario se actualicen.
+    """
+    # Implementación simulada para caja negra
+    sincronizacion_exitosa = True
+    datos_actualizados = True
+    return sincronizacion_exitosa, datos_actualizados
+
+# Caso de prueba 9: Confirmación de Pedido
+def confirmar_pedido():
+    """
+    Simula la confirmación de un pedido de productos.
+    Verifica que el pedido se confirme correctamente y se actualice el estado del inventario.
+    """
+    # Implementación simulada para caja negra
+    pedido_confirmado = True
+    estado_inventario_actualizado = True
+    return pedido_confirmado, estado_inventario_actualizado
+
+# Caso de prueba 10: Acceso de Usuario no Autorizado
+def acceso_usuario_no_autorizado():
+    """
+    Simula el intento de acceso a funciones de modificación del inventario con un usuario no autorizado.
+    Verifica que se deniegue el acceso y se muestre un mensaje de error.
+    """
+    # Implementación simulada para caja negra
+    acceso_denegado = True
+    mensaje_error = "Acceso no autorizado" if not acceso_denegado else ""
+    return acceso_denegado, mensaje_error
+
+# Caso de prueba 11: Manejo de Error en Comunicación con el Proveedor
+def manejar_error_comunicacion_proveedor(error_simulado):
+    """
+    Simula el manejo de errores de comunicación con el proveedor.
+    Verifica que el sistema registre el error y muestre un mensaje de error al usuario.
+    """
+    # Implementación simulada para caja negra
+    error_registrado = True
+    mensaje_error = "Error de comunicación con el proveedor" if error_simulado else ""
+    return error_registrado, mensaje_error
+
+# Caso de prueba 12: Publicación de Reseña de Producto
+def publicar_resena_producto(resena):
+    """
+    Simula la publicación de una reseña de producto por parte de los usuarios.
+    Verifica que la reseña se publique correctamente y esté disponible en la página del producto.
+    """
+    # Implementación simulada para caja negra
+    reseña_publicada = True
+    return reseña_publicada
+
+# Caso de prueba 13: Prueba de Compatibilidad con Navegadores
+def probar_compatibilidad_navegadores():
+    """
+    Simula la prueba de compatibilidad con diferentes navegadores web.
+    Verifica que las funciones del sistema de inventario se comporten correctamente en cada navegador.
+    """
+    # Implementación simulada para caja negra
+    compatibilidad_correcta = True
+    return compatibilidad_correcta
+
+# Caso de prueba 14: Búsqueda de producto por Nombre
+def buscar_producto_por_nombre(nombre_buscado, resultados_esperados):
+    """
+    Simula la funcionalidad de búsqueda de productos por nombre en el sistema de inventario.
+    Verifica que el sistema muestre el producto buscado en los resultados de búsqueda.
+    """
+    # Implementación simulada para caja negra
+    producto_encontrado = nombre_buscado in resultados_esperados
+    return producto_encontrado
+
+# Caso de prueba 15: Compra de producto con descuento
+def comprar_producto_con_descuento(nivel_stock, precio_original):
+    """
+    Simula la compra de un producto con descuento en el proceso de compra.
+    Verifica que el precio del producto refleje el descuento en el resumen de compra.
+    """
+    # Implementación simulada para caja negra
+    precio_final = precio_original - 10 if nivel_stock >= 10 else precio_original
+    return precio_final
+
+# Caso de prueba 16: Prueba de notificación al alcanzar niveles mínimos de inventario
+def probar_notificacion_niveles_minimos_inventario(inventario_alcanza_minimos):
+    """
+    Simula la notificación al alcanzar niveles mínimos de inventario.
+    Verifica que el sistema envíe una notificación a los administradores.
+    """
+    # Implementación simulada para caja negra
+    notificacion_enviada = inventario_alcanza_minimos
+    return notificacion_enviada
--- sodimac/tests/test_casos_de_uso.py
+++ sodimac/tests/test_casos_de_uso.py
@@ -1,8 +1,102 @@
-from ..black_box.casos_de_uso import iniciar_sesion, cerrar_sesion
+from ..black_box.casos_de_uso import (crear_pedido_producto, notificar_niveles_minimos_inventario,
+                        ejecutar_algoritmo_reabastecimiento, validar_cantidad_producto_orden_compra,
+                        calcular_disponibilidad_productos, aplicar_descuento_por_nivel_stock,
+                        sincronizar_con_proveedor, confirmar_pedido, acceso_usuario_no_autorizado,
+                        manejar_error_comunicacion_proveedor, publicar_resena_producto,
+                        probar_compatibilidad_navegadores, buscar_producto_por_nombre,
+                        comprar_producto_con_descuento, probar_notificacion_niveles_minimos_inventario,
+                        agregar_producto_al_inventario)
 import pytest
 
-def test_iniciar_sesion():
-    assert iniciar_sesion() == True
+# tests
 
-def test_cerrar_sesion():
-	assert cerrar_sesion() == True
+# Caso de prueba 1: Agregar Producto al Inventario
+def test_agregar_producto_al_inventario():
+    producto_agregado = agregar_producto_al_inventario({"nuevo_producto": "Producto de prueba"})
+    assert producto_agregado == True
+
+# Caso de prueba 2: Crear Pedido de Producto
+def test_crear_pedido_producto():
+    pedido_creado, inventario_afectado = crear_pedido_producto({"datos_validos": True})
+    assert pedido_creado == True
+    assert inventario_afectado == True
+
+# Caso de prueba 3: Notificar Niveles Mínimos de Inventario
+def test_notificar_niveles_minimos_inventario():
+    notificacion_enviada = notificar_niveles_minimos_inventario(True)
+    assert notificacion_enviada == True
+
+# Caso de prueba 4: Ejecutar Algoritmo de Reabastecimiento
+def test_ejecutar_algoritmo_reabastecimiento():
+    productos_reabastecidos = ejecutar_algoritmo_reabastecimiento()
+    assert productos_reabastecidos == True
+
+# Caso de prueba 5: Validar Cantidad de Producto en Orden de Compra
+def test_validar_cantidad_producto_orden_compra():
+    cantidad_valida, mensaje_error = validar_cantidad_producto_orden_compra(10)
+    assert cantidad_valida == True
+    assert mensaje_error == ""
+
+# Caso de prueba 6: Calcular Disponibilidad de Productos
+def test_calcular_disponibilidad_productos():
+    disponibilidad = calcular_disponibilidad_productos(100, 20)
+    assert disponibilidad == 80
+
+# Caso de prueba 7: Aplicar Descuento por Nivel de Stock
+def test_aplicar_descuento_por_nivel_stock():
+    descuento_aplicado = aplicar_descuento_por_nivel_stock(15)
+    assert descuento_aplicado == True
+
+# Caso de prueba 8: Sincronización con Proveedor
+def test_sincronizar_con_proveedor():
+    sincronizacion_exitosa, datos_actualizados = sincronizar_con_proveedor()
+    assert sincronizacion_exitosa == True
+    assert datos_actualizados == True
+
+# Caso de prueba 9: Confirmación de Pedido
+def test_confirmar_pedido():
+    pedido_confirmado, estado_inventario_actualizado = confirmar_pedido()
+    assert pedido_confirmado == True
+    assert estado_inventario_actualizado == True
+
+# Caso de prueba 10: Acceso de Usuario no Autorizado
+def test_acceso_usuario_no_autorizado():
+    acceso_denegado, mensaje_error = acceso_usuario_no_autorizado()
+    assert acceso_denegado == True
+    assert mensaje_error == ""
+
+# Caso de prueba 11: Manejo de Error en Comunicación con el Proveedor
+def test_manejar_error_comunicacion_proveedor():
+    error_registrado, mensaje_error = manejar_error_comunicacion_proveedor(True)
+    assert error_registrado == True
+    assert mensaje_error == "Error de comunicación con el proveedor"
+
+# Caso de prueba 12: Publicación de Reseña de Producto
+def test_publicar_resena_producto():
+    reseña_publicada = publicar_resena_producto("Buena reseña")
+    assert reseña_publicada == True
+
+# Caso de prueba 13: Prueba de Compatibilidad con Navegadores
+def test_probar_compatibilidad_navegadores():
+    compatibilidad_correcta = probar_compatibilidad_navegadores()
+    assert compatibilidad_correcta == True
+
+# Caso de prueba 14: Búsqueda de producto por Nombre
+def test_buscar_producto_por_nombre():
+    resultado = buscar_producto_por_nombre("Producto de prueba", ["Producto de prueba", "Otro producto"])
+    assert resultado == True
+
+# Caso de prueba 15: Compra de producto con descuento
+def test_comprar_producto_con_descuento():
+    precio_final = comprar_producto_con_descuento(15, 100)
+    assert precio_final == 90
+
+# Caso de prueba 16: Prueba de notificación al alcanzar niveles mínimos de inventario
+def test_probar_notificacion_niveles_minimos_inventario():
+    notificacion_enviada = probar_notificacion_niveles_minimos_inventario(True)
+    assert notificacion_enviada == True
+
+# Caso de prueba 1: Agregar Producto al Inventario
+def test_agregar_producto_al_inventario():
+    producto_agregado = agregar_producto_al_inventario({"nuevo_producto": "Producto de prueba"})
+    assert producto_agregado == True
